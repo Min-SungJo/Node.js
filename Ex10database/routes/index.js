43da9d4b0cb034e5b99f86df202d1a032e632b36
Min-SungJo <123or@naver.com>
2022-11-25 16:22:53 +0900

    DB실습 CRUD

--- Ex10database/routes/index.js
+++ Ex10database/routes/index.js
@@ -5,6 +5,7 @@ const db_config = require('../config/database');
 let conn = db_config.init();
 db_config.connect(conn);
 
+// 회원조회
 router.get('/select', (req, res) => {
 
     let sql = 'select * from member';
@@ -19,6 +20,7 @@ router.get('/select', (req, res) => {
     })
 })
 
+// 회원가입
 router.post('/insert', (req, res) => {
     let { id, pw, nick } = req.body;
     // let id = req.body.id
@@ -32,10 +34,53 @@ router.post('/insert', (req, res) => {
         if (err) { // 실패
             console.error(`insert 실행 실패! : ${err}`);
         } else { // 성공
-            res.redirect('main/select');
+            res.redirect('/main/select');
         }
-        res.send('INSERT_COMPLETE')
     });
 });
+//사용자가 요청하는 값이 바뀐다는 것을 알려주는 :
+router.get('/select/:id',(req,res)=>{
+    let id = req.params.id;
+    let sql = 'select * from member where id = ?';
+    conn.query(sql,[id],function(err,rows,fields){
+        console.log(rows);
+        console.log(fields);
+        if (err){
+            console.error(`select 실행 실패! : ${err}`);
+        } else {
+            res.json({listOne : rows});
+        }
+        res.send('SELECT_COMPLETE');
+    })
+})
 
+// 회원삭제
+router.get('/delete/:id',(req,res)=>{
+    let id = req.params.id;
+    let sql = 'delete from member where id = ?'
+    conn.query(sql,[id],function(err,rows,fields){
+        console.log(rows);
+        console.log(fields);
+        if (err){
+            console.error(`delete 삭제 실패! : ${err}`);
+        } else {
+            res.redirect('/main/select');
+        }
+    })
+})
+
+// 회원수정
+router.post('/update',(req,res)=>{
+    let { id, pw, nick } = req.body;
+    let sql = 'update member set pw = ?, nick = ? where id = ?'
+    conn.query(sql,[pw,nick,id],function(err,rows,fields){
+        console.log(rows);
+        console.log(fields);
+        if (err){
+            console.error(`update 수정 실패! : ${err}`);
+        } else {
+            res.redirect('/main/select');
+        }
+    })
+})
 module.exports = router;
